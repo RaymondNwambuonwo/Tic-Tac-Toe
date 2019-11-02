f266b618547b972455a01ae782605c6647d65651
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-11-02 18:07:42 -0400

    complete

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let boxTings = document.querySelectorAll('.item')
 console.log(boxTings)
-let player1 = "red"
-let player2 = "blue"
+// let player1 = "red"
+// let player2 = "blue"
 let counter1 = 0
 const box1 = document.querySelector("#item1")
 const box2 = document.querySelector("#item2")
@@ -12,24 +12,65 @@ const box6 = document.querySelector("#item6")
 const box7 = document.querySelector("#item7")
 const box8 = document.querySelector("#item8")
 const box9 = document.querySelector("#item9")
+let ptag = document.querySelector(".ptag")
+
+// for (let i = 0; i < boxTings.length; i++) {
+//     boxTings[i].style.backgroundColor = 'white'
+//     boxTings[i].addEventListener("click", function (e){
+//         e.preventDefault();
+//         if (e.target.style.backgroundColor === "white") {
+//         if (counter1 % 2 === 1) {
+//              boxTings[i].style.backgroundColor = "red"
+//              counter1 += 1
+//         } else {
+//             boxTings[i].style.backgroundColor = "blue"
+//             counter1 += 1
+//         }
+//     }
+//     })
+// }
+
 
 for (let i = 0; i < boxTings.length; i++) {
-    boxTings[i].style.backgroundColor = 'white'
-    boxTings[i].addEventListener('click', function (e){
-        e.preventDefault();
-        if (e.target.style.backgroundColor === "white") {
-        if (counter1 % 2 === 1) {
-             boxTings[i].style.backgroundColor = "red"
-             counter1 += 1
-        } else {
-            boxTings[i].style.backgroundColor = "blue"
-            counter1 += 1
+    boxTings[i].addEventListener("click", function (evt) {
+        evt.preventDefault()
+        console.log(evt)
+        if (evt.target.style.backgroundColor === "blue" || evt.target.style.backgroundColor === "red") {
+            return
         }
-    }
-    })
+        if (counter1 % 2 === 1) {
+            evt.target.style.backgroundColor = "red"
+            counter1 += 1
+            ptag.innerText = ("It's player1's turn")
+        }
+        else if (counter1 % 2 === 0) {
+            evt.target.style.backgroundColor = "blue"
+            counter1 += 1
+            ptag.innerText = ("It's player2's turn")
+        }
+})
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+var resetButton = document.getElementById('reset')
+resetButton.addEventListener("click", function(evt){
+    evt.preventDefault();
+    for (let i=0; i<boxTings.length; i++){
+      boxTings[i].style.backgroundColor = 'white';
+    }
+})
 
 
 function winGame() {
@@ -60,12 +101,11 @@ function winGame() {
         boxTings[z].style.backgroundColor = "white"
         }
     }
+     else if (click === 9){
+        alert("There has been a tie")
+        for (let j = 0; j < boxTings.length; j++) {
+        boxTings[j].style.backgroundColor = "white"
+        }
+    }
 }
 
-var resetButton = document.getElementById('reset')
-resetButton.addEventListener('click', function(evt){
-    evt.preventDefault();
-    for (let i=0; i<boxTings.length; i++){
-      boxTings[i].style.backgroundColor = 'white';
-    }
-})
